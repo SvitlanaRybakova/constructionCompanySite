8a7069880727103e809a5721c662ef6725c285ba
Svitlana <devop.edu@gmail.com>
2021-08-20 15:02:56 +0200

    implement modal window

--- js/main.js
+++ js/main.js
@@ -3,8 +3,13 @@ $(document).ready(function () {
   var currentFloor = 2;
   var counterUp = $(".counter-up");
   var counterDown = $(".counter-down");
+  var modal = $(".modal");
+  var floorPath = $(".home-img path");
+  var modalCloseBtn = $(".modal-close-button");
+  var viewFlatsBtn = $(".view-flats");
+
   // изменение в счетчике при наведении мышью на этаж
-  $(".home-img path").on("mouseover", function () {
+  floorPath.on("mouseover", function () {
     // удаляю подсветку этажа,чтобы при ховер эффекте удалить подстветку в случае когда этаж выбран с помощью кнопки
     $(".home-img path").removeClass("current-floor");
     // console.log($(this).attr("data-floor"));
@@ -14,7 +19,6 @@ $(document).ready(function () {
 
   // при клике на кнопку, подвсечивать этаж на картинке и менять counter
   counterUp.on("click", function () {
-    
     if (currentFloor < 18) {
       currentFloor++;
       // добавить 0 к счетчику (02, 03, 04...)
@@ -24,15 +28,13 @@ $(document).ready(function () {
       });
       $(".counter").text(usCurrentFloor);
       // удаляю подсветку этажа
-      $(".home-img path").removeClass("current-floor");
+      floorPath.removeClass("current-floor");
       // подсвечиваю этаж
-      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor")
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
     }
-    }
-  );
+  });
 
   counterDown.on("click", function () {
-   
     if (currentFloor > 2) {
       currentFloor--;
       // добавить 0 к счетчику (02, 03, 04...)
@@ -42,10 +44,21 @@ $(document).ready(function () {
       });
       $(".counter").text(usCurrentFloor);
       // удаляю подсветку этажа
-      $(".home-img path").removeClass("current-floor");
+      floorPath.removeClass("current-floor");
       // подсвечиваю этаж
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
     }
   });
 
+  floorPath.on('click', function() {
+    modal.toggleClass('is-open');
+  });
+
+  modalCloseBtn.on('click', function(){
+    modal.removeClass('is-open');
+  });
+
+  viewFlatsBtn.on("click", function () {
+    modal.toggleClass("is-open");
+  });
 });
